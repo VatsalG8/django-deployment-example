983515341b92738336908e7dafaf07697705765c
VatsalG8 <83281502+VatsalG8@users.noreply.github.com>
2021-06-25 11:32:23 +0530

    Update urls.py

--- learning_templates/learning_templates/urls.py
+++ learning_templates/learning_templates/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import url, include
 from basic_app import views
 
 urlpatterns = [
-    path(r'^$', views.index, name="index"),
-    path(r'^admin/', admin.site.urls),
-    path(r'^basic_app/',include('basic_app.urls')),
+    url(r'^$', views.index, name="index"),
+    url(r'^admin/', admin.site.urls),
+    url(r'^basic_app/',include('basic_app.urls')),
 ]
